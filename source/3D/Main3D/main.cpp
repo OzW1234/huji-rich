ba6ed0ab9cbd81eb5d5de27caa10dc5e687bfbe8
Elad Steinberg <eladtan1@gmail.com>
2015-04-07 10:34:59 +0300

    Changed the main test file to be a physical one

--- source/3D/Main3D/main.cpp
+++ source/3D/Main3D/main.cpp
@@ -1,13 +1,83 @@
 #include <iostream>
 #include <string>
+#include "../../newtonian/three_dimensional/eulerian_3d.hpp"
+#include "../../newtonian/three_dimensional/CourantFriedrichsLewy.hpp"
+#include "../../newtonian/three_dimensional/hdsim_3d.hpp"
+#include "../../newtonian/three_dimensional/FirstOrderHydroFlux.hpp"
+#include "../../newtonian/three_dimensional/3D_hdf5.hpp"
+#include "../GeometryCommon/OuterBoundary3D.hpp"
+#include "../../newtonian/three_dimensional/default_cell_updater.hpp"
+#include "../Voronoi/TetGenTessellation.hpp"
+#include "../../newtonian/common/hllc.hpp"
+#include "../../newtonian/common/ideal_gas.hpp"
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
+
 
 using namespace std;
 
+vector<Vector3D> RandSquare(int PointNum, Vector3D const& bll, Vector3D const& fur)
+{
+	typedef boost::mt19937_64 base_generator_type;
+	double ran[3];
+	vector<Vector3D> res;
+	Vector3D point;
+	base_generator_type generator;
+	boost::random::uniform_real_distribution<> dist;
+	for (int i = 0; i < PointNum; ++i)
+	{
+		ran[0] = dist(generator);
+		ran[1] = dist(generator);
+		ran[2] = dist(generator);
+		point.x = ran[0] * (fur.x-bll.x) + bll.x;
+		point.y = ran[1] * (fur.y - bll.y) + bll.y;
+		point.z = ran[2] * (fur.z - bll.z) + bll.z;
+		res.push_back(point);
+	}
+	return res;
+}
 
 void main()
 {
-	cout << "Done" << endl;
+	int nx=20, ny = 20, nz = 20;
+	Vector3D bll(-1, -1, -1), fur(1, 1, 1);
+	RectangularBoundary3D outer(fur, bll);
+	vector<Vector3D> points = RandSquare(nx*ny*nz, bll, fur);
+	TetGenTessellation<CloseToBoundaryGhostBuster> tess;
+	tess.Initialise(points, outer);
 
+	IdealGas eos(5. / 3.);
+	Hllc rs;
+	Eulerian3D pm;
+	FirstOrderHydroFlux flux_calc(rs);
+	CourantFriedrichsLewy timestep(0.3);
+	DefaultCellUpdater cell_update;
+
+	vector<ComputationalCell> cells(tess.GetPointNo());
+	double R_blast = 0.2;
+	for (int i = 0; i < tess.GetPointNo(); ++i)
+	{
+		cells[i].density = 1;
+		cells[i].velocity = Vector3D(0, 0, 0);
+		if (abs(tess.GetMeshPoint(i)) < R_blast)
+			cells[i].pressure = 10000;
+		else
+			cells[i].pressure = 1;
+	}
+
+	// Construct the simulation
+	HDSim3D sim(tess,cells, eos, pm, timestep, flux_calc, cell_update);
+
+	//Run the simulation
+	for (int i = 0; i < 40; ++i)
+	{
+
+		cout << i << endl;
+		sim.timeAdvance();
+	}
+
+	// Output the data
+	write_snapshot_to_hdf5(sim, "c:/sim_data/final.h5");
 #ifdef _DEBUG
 	cout << "Done, press ENTER to finish" << endl;
 	string s;
