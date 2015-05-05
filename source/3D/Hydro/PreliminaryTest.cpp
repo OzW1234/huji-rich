ae77dd79555a1f983a571a1b68c111064f7d9d5f
Itay Zandbank <zmbq@platonix.com>
2015-05-05 14:13:34 +0300

    Remove the 3D Hydro folder - it did not belong there.

--- source/3D/Hydro/PreliminaryTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/optional.hpp>
-#include "../Voronoi/TetGenTessellation.hpp"
-#include "../Voronoi/GhostBusters.hpp"
-
-void func()
-{
-	TetGenTessellation<CloseToBoundaryGhostBuster> voronoi;
-	voronoi.Initialise(vector<Vector3D>(), RectangularBoundary3D(Vector3D(1, 1, 1), Vector3D(0, 0, 0)));
-}
